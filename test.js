9b886cd7fd3bcbe7d266e766562bfa66e79c872c
LukeeZe <elcorazon9@hotmail.com>
2023-07-02 22:07:55 +0200

    hello

---
+++ test.js
@@ -0,0 +1,22 @@
+//if ( sessiontext == '2' && sessionnow == '2') {
+    return
+ //////   objTimer.sessionResetIsLive = true;
+ //    timertext = '5'
+  //  timertext2 = '0'
+  //  timertext3 = '0'
+    
+    
+ //   timer.innerHTML = `${timertext}:${timertext2}${timertext3}`
+ //   objTimer.sessionIsLive = true
+ //  }
+ //  else {
+ //   objTimer.sessionResetIsLive = false;
+ //   console.log(sessiontext)
+ //   sessiontext = parseInt(sessiontext) + 1;
+ //   sessiontext = sessiontext.toString()
+ //   h4session1.innerHTML = `${sessiontext} of 4`
+    
+    
+ //  }
+
+ 
